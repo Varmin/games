4a6a086381d077301476a3457fe5c3ef6cdb14af
yang <varmin@163.com>
2020-07-02 16:42:40 +0800

    ButterKnife

--- app/ModuleBase/src/main/java/com/atoshi/modulebase/BaseActivity.kt
+++ app/ModuleBase/src/main/java/com/atoshi/modulebase/BaseActivity.kt
@@ -1,11 +1,31 @@
 package com.atoshi.modulebase
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import butterknife.ButterKnife
+import butterknife.Unbinder
 
 /**
  * created by HYY on 2020/7/2
  * description:
  */
-open class BaseActivity : AppCompatActivity() {
+open abstract class BaseActivity : AppCompatActivity() {
+    private lateinit var mUnbinder: Unbinder
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId())
+        mUnbinder = ButterKnife.bind(this)
+        initData()
+        initView()
+    }
+
+    abstract fun getLayoutId(): Int
+    abstract fun initData()
+    abstract fun initView()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mUnbinder.unbind()
+    }
 }
--- app/ModuleLogin/src/main/java/com/atoshi/modulelogin/MainActivityLogin.kt
+++ app/ModuleLogin/src/main/java/com/atoshi/modulelogin/MainActivityLogin.kt
@@ -1,12 +1,12 @@
 package com.atoshi.modulelogin
 
-import android.os.Bundle
 import com.atoshi.modulebase.BaseActivity
 
 class MainActivityLogin : BaseActivity() {
+    override fun getLayoutId(): Int = R.layout.activity_main_login
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_login)
+    override fun initData() {
+    }
+    override fun initView() {
     }
 }
