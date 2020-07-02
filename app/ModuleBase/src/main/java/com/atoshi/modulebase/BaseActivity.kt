64093b2060e7367c9a122a743d64a23c814283e7
yang <varmin@163.com>
2020-07-02 15:42:37 +0800

    modules

---
+++ app/ModuleBase/src/main/java/com/atoshi/modulebase/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.atoshi.modulebase
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * created by HYY on 2020/7/2
+ * description:
+ */
+open class BaseActivity : AppCompatActivity() {
+
+}
---
+++ app/ModuleLogin/src/main/java/com/atoshi/modulelogin/MainActivityLogin.kt
@@ -0,0 +1,12 @@
+package com.atoshi.modulelogin
+
+import android.os.Bundle
+import com.atoshi.modulebase.BaseActivity
+
+class MainActivityLogin : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_login)
+    }
+}
