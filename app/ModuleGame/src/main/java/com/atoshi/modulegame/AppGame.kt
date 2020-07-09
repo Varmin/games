c0e762f0942b6b0f74d529e6d03456a19e410707
yang <varmin@163.com>
2020-07-09 16:09:21 +0800

    fix

---
+++ app/ModuleGame/src/main/java/com/atoshi/modulegame/AppGame.kt
@@ -0,0 +1,28 @@
+package com.atoshi.modulegame
+
+import android.app.Application
+import com.tencent.smtt.sdk.QbSdk
+
+/**
+ * created by HYY on 2020/7/3
+ * description:
+ */
+class AppGame: Application() {
+    private val TAG = "GameApp"
+    override fun onCreate() {
+
+        super.onCreate()
+        Thread(Runnable {
+            QbSdk.initX5Environment(this, object : QbSdk.PreInitCallback{
+                override fun onCoreInitFinished() {
+                    println("$TAG.onCoreInitFinished: ")
+                }
+
+                override fun onViewInitFinished(p0: Boolean) {
+                    println("$TAG.onViewInitFinished: $p0")
+                }
+
+            })
+        }).start()
+    }
+}
---
+++ app/src/main/java/com/atoshi/games/App.kt
@@ -0,0 +1,14 @@
+package com.atoshi.games
+
+import android.app.Application
+
+/**
+ * created by HYY on 2020/7/9
+ * description:
+ */
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
