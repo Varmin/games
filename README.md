5f735a643462d42496e2b17aba2fd8a768b7bf8b
yang <varmin@163.com>
2020-07-16 13:43:21 +0800

    Login

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 ## TODO
 * API动态更换url
+* ApiService返回值：Call可以手动控制异步、Observable在哪控制的？
+```
+Api.service.testGet2()
+
+    // TODO: by HY, 2020/7/15 封装线程切换   
+    .subscribeOn(Schedulers.io())
+    .observeOn(AndroidSchedulers.mainThread())
+
+    // TODO: by HY, 2020/7/15 传入封装类，返回Observer 
+    .subscribe(object : BaseObserver<List<TestBean>>() {
+    override fun onSuccess(data: List<TestBean>) {
+        println("${javaClass.simpleName}.onSuccess: ${data?.size}, ${Thread.currentThread().name}")
+    }
+})
+```
+* ButterKnife能引入，不能解析BindView；
+    * Kotlin中虽然使用extensions有些不太方便，但毕竟已经有了，是否可以有其它更好的方法？
+    * 即使extensions可以替代，但是不能bindclick，一样很麻烦
--- app/ModuleLogin/src/main/java/com/atoshi/modulelogin/MainActivityLogin.kt
+++ app/ModuleLogin/src/main/java/com/atoshi/modulelogin/MainActivityLogin.kt
@@ -1,20 +1,37 @@
 package com.atoshi.modulelogin
 
+import android.widget.ImageView
 import com.atoshi.modulebase.BaseActivity
 import com.atoshi.modulebase.net.Api
 import com.atoshi.modulebase.net.model.BaseObserver
 import com.atoshi.modulebase.net.model.TestBean
+import com.atoshi.modulebase.wx.WXUtils
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main_login.*
 
 class MainActivityLogin : BaseActivity() {
+
+    // TODO: by HY, 2020/7/15 BindView、BindClick
+    private lateinit var ivWxLogin: ImageView
+
     override fun getLayoutId(): Int = R.layout.activity_main_login
 
-    override fun initData() {}
+    override fun initData() {
+        WXUtils.registerApp(this)
+    }
     override fun initView() {
+        // TODO: by HY, 2020/7/15 顶层函数
+        iv_login_wx.setOnClickListener {
+            WXUtils.login()
+        }
+
         Api.service.testGet2()
-            //todo 切换
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
             .subscribe(object : BaseObserver<List<TestBean>>() {
             override fun onSuccess(data: List<TestBean>) {
-                println("${javaClass.simpleName}.onSuccess: ${data?.size} ")
+                println("${javaClass.simpleName}.onSuccess: ${data.size} ")
             }
         })
     }
